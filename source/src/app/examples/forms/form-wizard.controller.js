221723346d306a548a71304ae6a7239b302d7cb4
Sudhindra P.  Chausalkar <sudhindrap.c@gmail.com>
2016-05-20 18:01:52 +0530

    - login dialog added to payment gateway

--- source/src/app/examples/forms/form-wizard.controller.js
+++ source/src/app/examples/forms/form-wizard.controller.js
@@ -6,8 +6,56 @@
         .controller('FormWizardController', FormWizardController);
 
     /* @ngInject */
-    function FormWizardController() {
+    function FormWizardController($scope, $mdDialog) {
         var vm = this;
-        vm.data = {};
+
+        console.log("Controller is correct");
+
+        $scope.click = function($event, dialog){
+        	console.log("Clicked");
+
+		  $mdDialog.show({
+		      controller: DialogController,
+		      templateUrl: 'app/examples/forms/dialogs.tmpl.html',
+		      parent: angular.element(document.body),
+		      targetEvent: $event,
+		      clickOutsideToClose:true
+		    });
+        }
+
+        $scope.click();
+
+
+        vm.createDialog = createDialog;
+        vm.newDialog = {
+            title: 'Are you sure?',
+            content: 'This will wipe your whole computer!',
+            ok: 'Agree',
+            cancel: 'Disagree'
+        };
+
+        function createDialog($event, dialog) {
+        	console.log("Got it");
+            $mdDialog.show(
+                $mdDialog.confirm()
+                .title("Hi")
+                .textContent("dialog.content")
+                .ok("OK")
+                .cancel("cancel")
+                .targetEvent($event)
+            );
+        }
     }
 })();
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
