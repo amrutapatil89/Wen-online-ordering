628fc978d0250e774d5f1aeb3c46f982d81a05c0
Chetan Purohit <chetan.purohit@veloziti.com>
2016-06-06 18:47:54 +0530

    Added grid view for address.

--- source/src/app/examples/dashboards/server/dashboard-server.controller.js
+++ source/src/app/examples/dashboards/server/dashboard-server.controller.js
@@ -6,8 +6,31 @@
         .controller('DashboardServerController', DashboardServerController);
 
     /* @ngInject */
-    function DashboardServerController($scope, $timeout, $mdToast) {
+    function DashboardServerController($scope, $timeout, $mdToast, $http, $mdDialog) {
         var vm = this;
+
+        // Get call for users
+        $http.get("http://52.23.209.206:3000/api/v1/13HRYK02HZM30/customers/+1-1110000000")
+        .then(function(response) {
+            $scope.userName = response.data.firstName + " " + response.data.lastName;
+            $scope.userAddress = response.data.vzMongoAddr;
+
+            // console.log("response: "+JSON.stringify(response));
+
+            // console.log("USER name: "+JSON.stringify($scope.userName));
+            // console.log("USERS: "+JSON.stringify($scope.userAddress));
+        });
+
+        function createDialog($event) {
+            $mdDialog.show({
+                controller: 'AddressDialogController',
+                controllerAs: 'vm',
+                templateUrl: 'app/examples/dashboards/server/address-dialog.tmpl.html',
+                targetEvent: $event,
+                focusOnOpen: false
+            });
+        }
+
         vm.disks = [{
             icon: 'zmdi zmdi-storage',
             name: 'Ubuntu 10.04 LTS Disk Image',
@@ -37,13 +60,13 @@
             data: [15, 5, 35, 45]
         };
 
-        $timeout(function() {
-            $mdToast.show(
-                $mdToast.simple()
-                .content('Server CPU at 100%!')
-                .position('bottom right')
-                .hideDelay(3000)
-            );
-        }, 5000);
+        // $timeout(function() {
+        //     $mdToast.show(
+        //         $mdToast.simple()
+        //         .content('Server CPU at 100%!')
+        //         .position('bottom right')
+        //         .hideDelay(3000)
+        //     );
+        // }, 5000);
     }
 })();
