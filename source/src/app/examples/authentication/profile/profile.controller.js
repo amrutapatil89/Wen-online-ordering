bc2e8dd5968a039feeab2800b57e9195e1214cf5
Chetan Purohit <chetan.purohit@veloziti.com>
2016-06-10 16:48:10 +0530

    - Added profile editing page, order history and addresses page.

--- source/src/app/examples/authentication/profile/profile.controller.js
+++ source/src/app/examples/authentication/profile/profile.controller.js
@@ -6,49 +6,57 @@
         .controller('ProfileController', ProfileController);
 
     /* @ngInject */
-    function ProfileController() {
+    function ProfileController($http, $scope, $rootScope, $mdDialog) {
+
         var vm = this;
-        vm.settingsGroups = [{
-            name: 'Account Settings',
-            settings: [{
-                title: 'Show my location',
-                icon: 'zmdi zmdi-pin',
-                enabled: true
-            },{
-                title: 'Show my avatar',
-                icon: 'zmdi zmdi-face',
-                enabled: false
-            },{
-                title: 'Send me notifications',
-                icon: 'zmdi zmdi-notifications-active',
-                enabled: true
-            }]
-        },{
-            name: 'Chat Settings',
-            settings: [{
-                title: 'Show my username',
-                icon: 'zmdi zmdi-account',
-                enabled: true
-            },{
-                title: 'Make my profile public',
-                icon: 'zmdi zmdi-account-box',
-                enabled: false
-            },{
-                title: 'Allow cloud backups',
-                icon: 'zmdi zmdi-cloud-upload',
-                enabled: true
-            }]
-        }];
-        vm.user = {
-            name: 'Christos',
-            email: 'info@oxygenna.com',
-            location: 'Sitia, Crete, Greece',
-            website: 'http://www.oxygenna.com',
-            twitter: 'oxygenna',
-            bio: 'We are a small creative web design agency \n who are passionate with our pixels.',
-            current: '',
-            password: '',
-            confirm: ''
-        };
+        vm.createDialog = createDialog;
+
+        //for showing dialog box for adding new address by Chetan Purohit
+        function createDialog($event) {
+
+            $mdDialog.show({
+                controller: 'AddressDialogController',
+                controllerAs: 'vm',
+                templateUrl: 'app/examples/dashboards/server/address-dialog.tmpl.html',
+                targetEvent: $event,
+                focusOnOpen: false
+            });
+        }
+
+        // Get call for users
+        $http.get("http://52.23.209.206:3000/api/v1/13HRYK02HZM30/customers/+1-1110000000")
+        .then(function(response) {
+
+            $rootScope.userDetails = response;
+            $scope.firstName = response.data.firstName;
+            $scope.lastName = response.data.lastName;
+            $scope.userName = $scope.firstName + " " + $scope.lastName;
+            $scope.email = response.data.vzMongoCust.email;
+            $scope.userAddress = response.data.vzMongoAddr;
+        });
+
+        // Get call for orders
+        $http.get("http://52.23.209.206:3000/api/v1/4y_TUf5Wl/customerOrders/+1-1110000000")
+        .then(function(response) {
+
+            $scope.orders = response.data.elements;
+        });
+
+        //this function is used for setting address as default
+        $scope.setAddressAsDefault = function(add) {
+
+            $rootScope.userDetails.data.vzMongoAddr.forEach(function(addr, addrIndex) {
+
+                if(addr.isDefault) {
+
+                    $rootScope.userDetails.data.vzMongoAddr[addrIndex].isDefault = false;
+                }
+
+                if(addr._id == add._id) {
+
+                    $rootScope.userDetails.data.vzMongoAddr[addrIndex].isDefault = true;
+                }
+            });
+        }
     }
 })();
